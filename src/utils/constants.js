bcd713d759f3b11ce1cf8dbde46a18e17bcd0b7d
VarunBudhani <2022.varun.budhani@ves.ac.in>
2025-06-09 18:47:03 +0530

    backend url change

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-// export const BASE_URL = "https://event-backend-phjw.onrender.com"
-export const BASE_URL = "http://localhost:3001";
+export const BASE_URL = "https://event-backend-phjw.onrender.com"
+// export const BASE_URL = "http://localhost:3001";
